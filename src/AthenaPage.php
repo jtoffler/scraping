3ecd5c1b325f06ab7e0ead46c0db3c29dc4375ac
jtoffler <jake.toffler@gmail.com>
2016-10-26 14:40:23 -0400

    Create AthenaPage.php

---
+++ src/AthenaPage.php
@@ -0,0 +1,17 @@
+<?php
+class AthenaPage extends Page{
+	public function parse(){
+		return $this->getListingURLs();
+	}
+	public function getListingURLs(){
+		$listingsArr = array();
+		$listings = $this->getTag("article");
+		echo sizeof($listings) . "<br>";
+		foreach ($listings as $house) {
+			echo $house->plaintext . "<br>";
+			$url = $house->find("a", 0)->href;
+			array_push($listingsArr, $url);
+			return $listingsArr;
+		}
+	}
+}
